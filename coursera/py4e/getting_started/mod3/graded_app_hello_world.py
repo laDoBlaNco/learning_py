657c5a03ae7541d5842089078a9e5bc531c7999f
ladoblanco <whitesidekevin@gmail.com>
2026-03-08 20:28:51 -0400

    Restarting py43

--- coursera/py4e/getting_started/mod3/graded_app_hello_world.py
+++
@@ -1,5 +0,0 @@
-# write a program that uses a print function to say 'hello world' as shown
-# in desired output
-
-# the code below almost works, but there was a type
-print("hello world")
---
+++ py_tutorial/informal_intro.py
@@ -0,0 +1,65 @@
+# informal_intro.py
+
+# this is the first comment
+spam = 1 # and this is the second comment
+text = "# This is not a comment because it's inside quotes."
+print(text)
+
+
+# NUMBERS
+print(2+2)
+print(50-5*6)
+print((50-5*6)/4)
+print(8/5) # division always returns a floating point number
+
+# to do floor division use //; to calculate the remainder use %
+print()
+print(17/3) # classic division returns a float
+print(17//3) # floor division returns an int and throws away the fractional part
+print(17%3) # the % operator returns the remainder of the division
+print(5*3+2) # 17
+print()
+
+# with python its possible to sue the ** operator to raise a number to a power
+print(5**2) # 5 squared
+print(2**7) # 2 to the power of 7
+print()
+
+# The equal sign = is used to assign a value to a variable. Afterwards, no result
+# is displayed before the next interative prompt (if in the interactive mode)
+width = 20
+height = 5*9
+print(width*height)
+print()
+
+# if a variable is not "defined" (assigned a value), trying to use it will give an error
+# print(n) # Error: try to access an undefined variable
+
+# There is full support for floating point; operators with mixed type operands convert the
+# integer operand to floating point
+print(4*3.75-1)
+print()
+
+# in addition to int and float, Python supports other types of numbers, such as Decimal and 
+# Fractions. Python also has built-in support for complex numbers and uses the j or J prefix
+# to indicate the imaginary part of a complex number
+
+# 3.1.2 - TEXT:
+print('spam eggs')  # single quotes
+print("Paris rabbit got your back :)! Yay!")  # double quotes
+print('1975') # digits and numerals enclosed in quotes are also strings
+print()
+
+#escaping special characters
+print('doesn\'t') # use \' to escape a single quote
+print("doesn't") # ... or use double quotes
+print('"Yes," they said')
+print("\"Yes,\" they said")
+print('"Isn\'t," they said.')
+print()
+
+
+
+
+
+
